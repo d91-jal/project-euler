4ef5ca69acd796ced7e12eb2c0eacb30b5bc985e
d91-jal <replokalen@gmail.com>
2014-02-16 10:26:20 +0100

    Move common functions to utils.py

---
+++ utils.py
@@ -0,0 +1,39 @@
+__author__ = 'johan'
+
+import math
+
+def is_pan_digital_string(text):
+    compare_to = ''
+    trim_text = text.strip().replace(' ', '')
+
+    if len(trim_text > 9):
+        return False
+
+    for i in range(1, len(trim_text) + 1):
+        compare_to += str(i)
+
+    return ''.join(sorted(trim_text)) == compare_to
+
+
+def is_pan_digital_number(num):
+    if num > 987654321:
+        return False
+
+    result = [0 for i in range(1, math.ceil(math.log(num, 10)) + 1)]
+
+    while num >= 1:
+        digit = num % 10
+
+        if digit == 0 or digit > len(result):
+            return False
+
+        result[digit - 1] = 1
+        num //= 10
+
+    return sum(result) == len(result)
+
+
+#print(is_pan_digital_number(1123456))
+#print(is_pan_digital_number(7123456))
+#print(is_pan_digital_number(1234567890))
+#print(is_pan_digital_number(123456))
