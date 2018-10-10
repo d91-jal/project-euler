221a35321a38d19139fa4e7c4c0fdc94fd59af7a
johan <replokalen@gmail.com>
2018-10-10 15:43:27 +0200

    Add solution approach for 48.

--- problems/048_SelfPowers.py
+++ problems/048_SelfPowers.py
@@ -1,6 +1,11 @@
 # The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 #
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+#
+# Approach: When going above a certain threshold, adding more exponentials to the sum should no longer
+# impact the last ten digits. Find that threshold and just sum until it has been reached.
+
+
 
 
 
